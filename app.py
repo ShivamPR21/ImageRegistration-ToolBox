907a78ee492d4eefeefad9c9bcdc488d2141e60a
ShivamPR21 <pandeyshivam2017robotics@gmail.com>
2021-04-07 04:22:02 +0530

    Transformation App GUI form init.
    Signed-off-by: ShivamPR21 <pandeyshivam2017robotics@gmail.com>

--- app.py
+++ app.py
@@ -23,55 +23,8 @@ def home_page():
     :return: render_template()
     """
     form = RegisterForm()
-    if form.validate_on_submit():
-        upload_path = os.path.join('static/uploads/data/', secure_filename(form.file_name.data.filename))
-        form.file_name.data.save(upload_path)
-        method = form.pca_method.data
 
-        cache_path = 'static/uploads/cache/'
-        # Read file from local upload
-        # try:
-        global img_proc
-        if method == 'std':
-            img_proc = ImageryProc(upload_path, cache_path)
-        elif method == 'nstd':
-            img_proc = ImageryProc(upload_path, cache_path, False)
-
-        img_proc.init_pca()
-        print("PCA Init")
-        img_proc.pca()
-        print("PCA computed")
-        img_proc.analyse()
-        print("Analyse PCA")
-        # except:
-        #     print("Some Error Occurred")
-        img_proc.pca_viz()
-
-        pcs_path = os.listdir('static/uploads/cache/pcs')
-        print(pcs_path)
-
-        return render_template('index.html', app_name='PCA App', form=form
-                               , shrink="col-2", show_form='', show_result='show',
-                               pcs_path=['uploads/cache/pcs/pc_0.png',
-                                         'uploads/cache/pcs/pc_1.png',
-                                         'uploads/cache/pcs/pc_2.png',
-                                         'uploads/cache/pcs/pc_3.png'],
-                               covar_path='uploads/cache/covar.png',
-                               pcs_covar_path='uploads/cache/pc.png',
-                               eig_val=img_proc.eigenval.ravel(),
-                               eig_vec=img_proc.eigenvec)
-
-    if form.errors != {}:  # If there are not errors from the validations
-        for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
-
-    return render_template('index.html', app_name='PCA App',
-                           form=form, shrink="col-2", pcs_path=['', '', '', ''],
-                           covar_path='',
-                           pcs_covar_path='',
-                           eig_val=[0, 0, 0, 0],
-                           eig_vec=[0, 0, 0, 0]
-                           )
+    return render_template('index.html', app_name='Affine App', form=form)
 
 
 if __name__ == '__main__':
--- irtb/forms.py
+++ irtb/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, FloatField, SubmitField
 
 
 class RegisterForm(FlaskForm):
@@ -10,8 +10,31 @@ class RegisterForm(FlaskForm):
     file_name = FileField(label='File Path:',
                           validators=[FileRequired()]
                           )
-    pca_method = SelectField(label='method:',
-                             choices=[('std', 'Standardised PCA'),
-                                      ('nstd', 'Non-Standardised PCA')]
-                             )
-    submit = SubmitField(label='Start Analysis')
+    translation_x = FloatField(label='Translation X:',
+                               validators=[FileRequired()]
+                               )
+    translation_y = FloatField(label='Translation Y:',
+                               validators=[FileRequired()]
+                               )
+    rotation = FloatField(label='Rotation:',
+                          validators=[FileRequired()]
+                          )
+    scale_x = FloatField(label='Scale X:',
+                         validators=[FileRequired()]
+                         )
+    scale_y = FloatField(label='Scale Y:',
+                         validators=[FileRequired()]
+                         )
+    skew_x = FloatField(label='Skew X:',
+                        validators=[FileRequired()]
+                        )
+    skew_y = FloatField(label='Skew Y:',
+                        validators=[FileRequired()]
+                        )
+    interpolation_method = SelectField(label='Interpolation Method:',
+                                       choices=[('nn', 'Nearest Neighbour'),
+                                                ('bi-linear', 'Bi-Linear'),
+                                                ('bi-cubic', 'Bi-Cubic')]
+                                       )
+
+    submit = SubmitField(label='Warp Image')
